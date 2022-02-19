8b95c38cdd801d944b5fde6cb4ccfc1ac2112230
QA Loop <agiletester786@gmail.com>
2022-02-19 22:06:49 +0530

    First Commit

---
+++ cypress/integration/api-tests/todo.spec.js
@@ -0,0 +1,31 @@
+describe('TODO api testing', () => {
+    let todoItem = 3;
+    it('fetches ToDo items - GET', () => {
+        cy.request('/todos/').as('todoRequest');
+        cy.get('@todoRequest').then(todos => {
+            expect(todos.status).to.eq(200);
+            assert.isArray(todos.body, 'Todos Reponse in an array')
+        });
+    });
+
+    it('deletes Todo items - DELETE', () => {
+        cy.request('DELETE', `/todos/${todoItem}`).as('todoRequest');
+        // deletes Todo item with id = 3
+        cy.get('@todoRequest').then(todos => {
+            expect(todos.status).to.eq(200);
+            assert.isString(todos.body, 'todo deleted!')
+        });
+    });
+
+    it('Adds Todo item - POST', () => {
+        cy.request('POST', '/todos/', { task: "run tests" }).as('todoRequest');
+        // adds new Todo item by defining Todo name
+        cy.get('@todoRequest').then(todos => {
+            expect(todos.status).to.eq(200);
+            cy.wrap(todos.body).should('deep.include', {
+                task: 'run tests',
+                completed: false,
+            });
+        });
+    });
+});
